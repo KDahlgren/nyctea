d1ef08504d1bedd05234b4c2b37f1bb2f6f3d747
KDahlgren <kdahlgren15@gmail.com>
2018-02-02 23:21:17 -0800

    built code for adding random facts to existing edbs and for add new random edb facts.

--- qa/Test_nyctea.py
+++ qa/Test_nyctea.py
@@ -16,7 +16,7 @@ import copy, inspect, logging, os, sqlite3, sys, time, unittest
 if not os.path.abspath( __file__ + "/../../src" ) in sys.path :
   sys.path.append( os.path.abspath( __file__ + "/../../src" ) )
 
-#import nyctea
+from nyctea import Nyctea
 
 if not os.path.abspath( __file__ + "/../../lib/iapyx/src" ) in sys.path :
   sys.path.append( os.path.abspath( __file__ + "/../../lib/iapyx/src" ) )
@@ -33,16 +33,195 @@ from evaluators import c4_evaluator
 #################
 class Test_nyctea( unittest.TestCase ) :
 
-  logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.DEBUG )
-  #logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.INFO )
+  #logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.DEBUG )
+  logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.INFO )
   #logging.basicConfig( format='%(levelname)s:%(message)s', level=logging.WARNING )
 
   PRINT_STOP = False
 
+  ###############
+  #  EXAMPLE 1  #
+  ###############
+  def test_nyctea_example_1( self ) :
+
+    test_id = "_nyctea_example_1_"
+
+    # --------------------------------------------------------------- #
+    # testing set up.
+
+    testDB = "./IR" + test_id + ".db"
+
+    if os.path.exists( testDB ) :
+      os.remove( testDB )
+
+    IRDB   = sqlite3.connect( testDB )
+    cursor = IRDB.cursor()
+
+    # --------------------------------------------------------------- #
+    #dependency
+
+    #dedt.createDedalusIRTables(cursor)
+    dedt.globalCounterReset()
+
+    # --------------------------------------------------------------- #
+
+    # specify input file path
+    inputfile = "./testFiles/example_1.ded"
+
+    # get argDict
+    argDict = self.getArgDict( inputfile )
+
+    # specify settings file
+    argDict[ "settings" ] = "./settingsFiles/settings_sync_assumption.ini"
+
+    # instantiate nyctea object
+    n = Nyctea.Nyctea( argDict, cursor )
+
+    # run translator
+    programData, factMeta, ruleMeta = n.run()
+
+    # portray actual output program lines as a single string
+    actual_results = self.getActualResults( programData[0] )
+
+    if self.PRINT_STOP :
+      print actual_results
+      sys.exit( "print stop." )
+
+    # grab expected output results as a string
+    expected_results_path = "./testFiles/example_1.olg"
+    expected_results      = None
+    with open( expected_results_path, 'r' ) as expectedFile :
+      expected_results = expectedFile.read()
+
+    self.assertEqual( actual_results, expected_results )
+
+    # --------------------------------------------------------------- #
+    #clean up testing
+
+    IRDB.close()
+
+    if os.path.exists( testDB ) :
+      os.remove( testDB )
+
+
+  ########################
+  #  NYCTEA CONSTRUCTOR  #
+  ########################
+  def test_nyctea_constructor( self ) :
+
+    test_id = "_test_nyctea_constructor_"
+
+    # --------------------------------------------------------------- #
+    # testing set up.
+    testDB = "./IR" + test_id + ".db"
+    IRDB   = sqlite3.connect( testDB )
+    cursor = IRDB.cursor()
+
+    # --------------------------------------------------------------- #
+    #dependency
+    #dedt.createDedalusIRTables(cursor)
+    dedt.globalCounterReset()
+
+    # --------------------------------------------------------------- #
+
+    # specify input file path
+    inputfile = "./testFiles/empty.ded"
+
+    # get argDict
+    argDict = self.getArgDict( inputfile )
+
+    # instantiate nyctea object
+    n = Nyctea.Nyctea( argDict, cursor )
+
+    expected_prov_diagrams            = False
+    expected_use_symmetry             = False
+    expected_crashes                  = 0
+    expected_solver                   = None
+    expected_disable_dot_rendering    = False
+    expected_settings                 = "./settings.ini"
+    expected_negative_support         = False
+    expected_strategy                 = None
+    expected_file                     = inputfile
+    expected_EOT                      = 4
+    expected_find_all_counterexamples = False
+    expected_nodes                    = [ "a", "b", "c" ]
+    expected_evaluator                = "c4"
+    expected_EFF                      = 2
+
+    self.assertEqual( argDict[ "prov_diagrams" ],            expected_prov_diagrams            )
+    self.assertEqual( argDict[ "use_symmetry" ],             expected_use_symmetry             )
+    self.assertEqual( argDict[ "crashes" ],                  expected_crashes                  )
+    self.assertEqual( argDict[ "solver" ],                   expected_solver                   )
+    self.assertEqual( argDict[ "disable_dot_rendering" ],    expected_disable_dot_rendering    )
+    self.assertEqual( argDict[ "settings" ],                 expected_settings                 )
+    self.assertEqual( argDict[ "negative_support" ],         expected_negative_support         )
+    self.assertEqual( argDict[ "strategy" ],                 expected_strategy                 )
+    self.assertEqual( argDict[ "file" ],                     expected_file                     )
+    self.assertEqual( argDict[ "EOT" ],                      expected_EOT                      )
+    self.assertEqual( argDict[ "find_all_counterexamples" ], expected_find_all_counterexamples )
+    self.assertEqual( argDict[ "nodes" ],                    expected_nodes                    )
+    self.assertEqual( argDict[ "evaluator" ],                expected_evaluator                )
+    self.assertEqual( argDict[ "EFF" ],                      expected_EFF                      )
+
+    # --------------------------------------------------------------- #
+    #clean up testing
+    IRDB.close()
+    os.remove( testDB )
+
+
+  # ////////////////////////////// #
+  #          HELPER TOOLS          #
+  # ////////////////////////////// #
+
+  ###############
+  #  GET ERROR  #
+  ###############
+  # extract error message from system info
+  def getError( self, sysInfo ) :
+    return str( sysInfo[1] )
+
+
+  ########################
+  #  GET ACTUAL RESULTS  #
+  ########################
+  def getActualResults( self, programLines ) :
+    program_string  = "\n".join( programLines )
+    program_string += "\n" # add extra newline to align with read() parsing
+    return program_string
+
+
+  ##################
+  #  GET ARG DICT  #
+  ##################
+  def getArgDict( self, inputfile ) :
+
+    # initialize
+    argDict = {}
+
+    # populate with unit test defaults
+    argDict[ 'prov_diagrams' ]            = False
+    argDict[ 'use_symmetry' ]             = False
+    argDict[ 'crashes' ]                  = 0
+    argDict[ 'solver' ]                   = None
+    argDict[ 'disable_dot_rendering' ]    = False
+    argDict[ 'settings' ]                 = "./settings.ini"
+    argDict[ 'negative_support' ]         = False
+    argDict[ 'strategy' ]                 = None
+    argDict[ 'file' ]                     = inputfile
+    argDict[ 'EOT' ]                      = 4
+    argDict[ 'find_all_counterexamples' ] = False
+    argDict[ 'nodes' ]                    = [ "a", "b", "c" ]
+    argDict[ 'evaluator' ]                = "c4"
+    argDict[ 'EFF' ]                      = 2
+
+    return argDict
 
 
 if __name__ == "__main__":
   unittest.main()
+  if os.path.exists( "./IR*.db*" ) :
+    os.remove( "./IR*.db*" )
+
 
 #########
 #  EOF  #
--- src/nyctea/nyctea.py
+++ src/nyctea/nyctea.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-nyctea.py
+Nyctea.py
 '''
 
 # **************************************** #
@@ -11,20 +11,238 @@ nyctea.py
 #  IMPORTS  #
 #############
 # standard python packages
-import inspect, itertools, logging, os, sqlite3, string, sys, time
+import copy, inspect, itertools, logging, os, sqlite3, string, sys, time, random
 
 # ------------------------------------------------------ #
 # import sibling packages HERE!!!
 if not os.path.abspath( __file__ + "/../../../lib/iapyx/src" ) in sys.path :
   sys.path.append( os.path.abspath( __file__ + "/../../../lib/iapyx/src" ) )
 
-from dedt           import dedt, dedalusParser
+from dedt           import Fact, dedt, dedalusParser
 from utils          import parseCommandLineInput, tools
 from evaluators     import c4_evaluator
+
+if not os.path.abspath( __file__ + "/../translators" ) in sys.path :
+  sys.path.append( os.path.abspath( __file__ + "/../translators" ) )
+
+from translators import c4_translator, dumpers_c4
 
 # **************************************** #
 
 
-####################
-#  CLASS #
-####################
+class Nyctea( object ) :
+
+  #################
+  #  CONSTRUCTOR  #
+  #################
+  # argDict := the dictionary of input arguments
+  # cursor  := the pointer to the database instance
+
+  def __init__( self, argDict, cursor ) :
+
+    self.argDict       = argDict
+    self.cursor        = cursor
+
+    logging.debug( "  NYCTEA CONSTRUCTOR : running nyctea with argDict :" )
+    for key in argDict :
+      logging.debug( "    argDict[ '" + key + "' = " + str( argDict[ key ] )  )
+
+    # ----------------------------------------------------------------- #
+    # adjust run parameters given inputs
+    # EOT, EFF, crashes
+
+    self.num_random_facts, self.num_random_edbs, self.random_edb_arity = self.adjust_params()
+
+    logging.debug( "  NYCTEA CONSTRUCTOR : self.num_random_facts = " + str( self.num_random_facts ) )
+    logging.debug( "  NYCTEA CONSTRUCTOR : self.num_random_edbs  = " + str( self.num_random_edbs ) )
+    logging.debug( "  NYCTEA CONSTRUCTOR : self.random_edb_arity = " + str( self.random_edb_arity) )
+
+
+  #########
+  #  RUN  #
+  #########
+  # translate the input ded file into c4 datalog while adding 
+  # additional input
+
+  def run( self ) :
+
+    logging.debug( "  NYCTEA RUN : running process..." )
+
+    # ----------------------------------------------------------------- #
+    # create IR tables
+
+    dedt.createDedalusIRTables( self.cursor )
+
+    # ----------------------------------------------------------------- #
+    # get all input files
+
+    starterFile        = self.argDict[ 'file' ]
+    fileList           = tools.get_all_include_file_paths( starterFile )
+    complete_file_path = tools.compile_full_program_and_save( fileList )
+
+    logging.debug( "  NYCTEA RUN : fileList           = " + str( fileList ) )
+    logging.debug( "  NYCTEA RUN : complete_file_path = " + str( complete_file_path ) )
+
+    # ----------------------------------------------------------------- #
+    # ded to IR
+
+    meta     = dedt.dedToIR( complete_file_path, self.cursor, self.argDict[ "settings" ] )
+    factMeta = meta[0]
+    ruleMeta = meta[1]
+
+    # ------------------------------------------------------------- #
+    # generate the first clock
+  
+    dedt.starterClock( self.cursor, self.argDict )
+
+    # ------------------------------------------------------------- #
+    # generate additional input
+
+    factMeta.extend( self.add_random_facts( factMeta ) )
+    factMeta.extend( self.add_random_edbs()  )
+  
+    # ------------------------------------------------------------- #
+    # apply rewrites to generate the corresponding datalog program
+    # in the IR database
+
+    allMeta = dedt.rewrite_to_datalog( self.argDict, factMeta, ruleMeta, self.cursor )
+
+    # ------------------------------------------------------------- #
+    # translate IR into c4 datalog
+
+    # programData[ 0 ] := all the c4 datalog lines
+    # programData[ 1 ] := program table list
+    programData = c4_translator.c4datalog( self.argDict, self.cursor )
+
+    logging.debug( "  NYCTEA RUN : programLData[0] :" )
+    for line in programData[ 0 ] :
+      logging.debug( "    " + line )
+    logging.debug( "  NYCTEA RUN : programData[1] : " )
+    for table in programData[ 1 ] :
+      logging.debug( "    " + table )
+
+    # ------------------------------------------------------------- #
+    # save all program lines to file
+
+    try :
+      if self.argDict[ "data_save_path" ] :
+        logging.debug( "  NYCTEA RUN : using data_save_path '" + self.argDict[ "data_save_path" ] + "'" )
+        fo = open( self.argDict[ "data_save_path" ] + "final_initial_program.olg", "w" )
+        for line in program[0] :
+          fo.write( line + "\n" )
+        fo.close()
+
+    except KeyError :
+      logging.debug( "  NYCTEA RUN : no data_save_path specified. skipping writing final olg program to file." )
+
+ 
+    logging.debug( "  NYCTEA RUN : ...done." )
+
+    return [ programData, factMeta, ruleMeta ]
+
+
+  ######################
+  #  ADD RANDOM FACTS  #
+  ######################
+  # add random acts to existing edbs
+  def add_random_facts( self, factMeta ) :
+
+    new_facts_meta = []
+
+    for fact in factMeta :
+
+      # get fact arity
+      arity = len( fact.factData[ 'dataList' ] )
+
+      # get types
+      type_list = []
+      for d in fact.factData[ 'dataList' ] :
+        if d.startswith( "'" ) and d.endswith( "'" ) :
+          type_list.append( "string" )
+        elif d.startswith( '"' ) and d.endswith( '"' ) :
+          type_list.append( "string" )
+        elif d.isdigit() :
+          type_list.append( "int" )
+        else :
+          raise ValueError( "  FATAL ERROR : unknown data type for '" + d + "' in fact '" + str( fact.factData ) + "'" )
+
+      for j in range( 0, self.num_random_facts ) :
+        relationName = fact.relationName
+        dataList     = []
+        factTimeArg  = "1"
+
+        for k in range( 0, arity ) :
+          int_att = random.randint( 0, 100 )
+          str_att = '"randomatt' + str( int_att ) + '"'
+          if type_list[ k ] == "int" :
+            dataList.append( int_att )
+          else :
+            dataList.append( str_att )
+
+        # save fact
+        factData = {}
+        factData[ 'relationName' ] = relationName
+        factData[ 'dataList' ]     = dataList
+        factData[ 'factTimeArg' ]  = factTimeArg
+  
+        fid            = tools.getIDFromCounters( "fid" )
+        newFact        = copy.deepcopy( Fact.Fact( fid, factData, self.cursor ) )
+        newFact.cursor = self.cursor
+        new_facts_meta.append( newFact )
+
+    return new_facts_meta
+
+
+  #####################
+  #  ADD RANDOM EDBS  #
+  #####################
+  # factData = { relationName:'relationNameStr', dataList:[ data1, ... , dataN ], factTimeArg:<anInteger> }
+  def add_random_edbs( self ) :
+
+    new_facts_meta = []
+
+    for i in range( 0, self.num_random_edbs ) :
+      relationName = "random_edb_number_" + str( i )
+      dataList     = []
+      factTimeArg  = "1"
+
+      # generate random data list
+      for j in range( 0, self.random_edb_arity ) :
+        int_att = random.randint( 0, 100 )
+        str_att = '"randomatt' + str( int_att ) + '"'
+        if random.random() > 0.5 :
+          dataList.append( int_att )
+        else :
+          dataList.append( str_att )
+
+      # save fact
+      factData = {}
+      factData[ 'relationName' ] = relationName
+      factData[ 'dataList' ]     = dataList
+      factData[ 'factTimeArg' ]  = factTimeArg
+
+      fid            = tools.getIDFromCounters( "fid" )
+      newFact        = copy.deepcopy( Fact.Fact( fid, factData, self.cursor ) )
+      newFact.cursor = self.cursor
+      new_facts_meta.append( newFact )
+
+    return new_facts_meta
+
+
+  ###################
+  #  ADJUST PARAMS  #
+  ###################
+  # adjust run parameters given nyctea configurations inputs for EOT, EFF, crashes
+  # in the settings file
+  def adjust_params( self ) :
+
+    num_random_facts = 10
+    num_random_edbs = 10
+    random_edb_arity = 5
+
+    return [ num_random_facts, num_random_edbs, random_edb_arity ]
+
+
+#########
+#  EOF  #
+#########
